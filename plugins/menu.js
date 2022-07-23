7a3fdd1f8dacacb552f7f65739335b966a26e131
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-07-23 23:56:04 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -13,16 +13,18 @@ exports.run = {
          rowId: `${isPrefix}menutype 4`,
          description: `Sticker & video & music Converter ❄ `
       }, {
-         title: 'اوامر اضافية',
+         title: 'أوامــر إضـــافية',
          rowId: `${isPrefix}menutype 6`,
-         description: ``
+         description: ` @wox_bella سيتم إضافة مميزات أخرى من طرف   `
       }]
        let text = '  بوبيز بوت هو بوت مغربي يقدم لك خدمات مميزة يمكن ان تساعدك في دراستك وفي حياتك اليومية ♥\n\n'
      
       text += '◦ *❀FACEBOOK* : https://www.facebook.com/profile.php?id=100063533185520\n'
+      
       text += '◦ *❀INSTAGRAM* : https://www.instagram.com/noureddine_ouafy\n\n'
+      
       text += ' اذا واجهتك اي مشكلة في البوت تواصل مع صاحب البوت ☺.'
-      await client.sendList(m.chat, '', text, '', ' MENU أنقرهنا ليظهر لك!', [{
+      await client.sendList(m.chat, '', text, '', 'MENU أنقر هنا ليظهر لـك', [{
          rows
       }], m)
    },
