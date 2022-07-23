cda6a0078007da02bdc30f913b63c2d072fa1b36
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-07-23 23:30:14 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,9 +9,7 @@ exports.run = {
          rowId: `${isPrefix}menutype 1`,
          description: `fb / youtube / tiktok / instagram ...`
       }, {
-         title: 'اوامر صاحب البوت في المجموعات',
-         rowId: `${isPrefix}menutype 2`,
-         description: ` لا تستعمل هذه الاوامر فهمي خاصة بصاحب البوت فقط `
+       
       }, {
          title: 'معلومات عن المستعمل',
          rowId: `${isPrefix}menutype 3`,
@@ -30,11 +28,11 @@ exports.run = {
          description: ``
       }]
        let text = '  بوبيز بوت هو بوت مغربي يقدم لك خدمات مميزة يمكن ان تساعدك في دراستك وفي حياتك اليومية ♥\n\n'
-      text += '◦ *❀DEVELOPER* : ɴᴏᴜʀᴇᴅᴅɪɴᴇ ᴏᴜᴀꜰʏ\n'
+     
       text += '◦ *❀FACEBOOK* : https://www.facebook.com/profile.php?id=100063533185520\n'
       text += '◦ *❀INSTAGRAM* : https://www.instagram.com/noureddine_ouafy\n\n'
       text += ' اذا واجهتك اي مشكلة في البوت تواصل مع صاحب البوت ☺.'
-      await client.sendList(m.chat, '', text, '', ' MENU أنقر هنا هنا ليظهر لك!', [{
+      await client.sendList(m.chat, '', text, '', ' MENU أنقرهنا ليظهر لك!', [{
          rows
       }], m)
    },
