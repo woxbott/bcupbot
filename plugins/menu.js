c8f361a776dbacc114d22e8b17aa7d4e3fb40c2f
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-08-02 00:09:14 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,43 +1,37 @@
 exports.run = {
-   usage: ['menu', 'help', 'bot', 'boubiz' , 'list' ],
+   usage: ['menu', 'help', 'bot'],
    async: async (m, {
       client,
       isPrefix
    }) => {
       let rows = [{
-         title: ': الـتحـميل مــن الـمـواقــع مثـال ',
+         title: 'DOWNLOADER',
          rowId: `${isPrefix}menutype 1`,
-         description: `fb / youtube / tiktok / instagram / mediafire ...`
+         description: ``
       }, {
-         title: 'قـسم تـحـويـل المـلـفـات (مــلصـقـات ..)',
+         title: 'GROUP TOOLS',
+         rowId: `${isPrefix}menutype 2`,
+         description: ``
+      }, {
+         title: 'USER INFO',
+         rowId: `${isPrefix}menutype 3`,
+         description: ``
+      }, {
+         title: 'UTILITIES',
          rowId: `${isPrefix}menutype 4`,
-         description: `Sticker & video & music Converter ❄ `
+         description: ``
       }, {
-         title: 'أوامــر إضـــافية',
+         title: 'OWNER TOOLS',
+         rowId: `${isPrefix}menutype 5`,
+         description: ``
+      }, {
+         title: 'SPECIAL',
          rowId: `${isPrefix}menutype 6`,
-         description: ` @wox_bella سيتم إضافة مميزات أخرى من طرف   `
+         description: ``
       }]
-       let text = '  بوبيز بوت هو بوت مغربي يقدم لك خدمات مميزة يمكن ان تساعدك في دراستك وفي حياتك اليومية ♥\n\n'
-     
-      text += '◦ *❀FACEBOOK* : https://www.facebook.com/profile.php?id=100063533185520\n'
-      
-      text += '◦ *❀INSTAGRAM* : https://www.instagram.com/noureddine_ouafy\n\n'
-      
-      text += ' اذا واجــهتك اي مشكلة في البــوت تـواصـل مع صـاحـب البوت ☺.'
-      await client.sendList(m.chat, '', text, '', 'MENU أنقر هنا ليظهر لـك', [{
+      await client.sendList(m.chat, '', global.db.setting.msg, '© neoxr-bot v2.2.0', 'Tap!', [{
          rows
       }], m)
-      
-      
-      
-       client.sendFile(m.chat, 'https://github.com/silana2016/neoxr-bot/blob/master/media/image/Boobiz%20(%20DRILL%20REMIX%20)%20(%20256kbps%20cbr%20).mp3?raw=true', 'MED.mp3', '', m, {
-          document: false
-       })
-      
-      
-      client.reply(m.chat, `boubiz bot by nourddine :)`, m)
-      
-      
    },
    error: false
 }
