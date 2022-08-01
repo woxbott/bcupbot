2f3115c9b5d90f5d93440ecf7643c5059b59ff82
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-08-02 00:11:47 +0100

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '212605784394'
+global.owner = '212632244668'
 // Owner name
-global.owner_name = 'ɴᴏᴜʀᴇᴅᴅɪɴᴇ_ᴏᴜᴀꜰʏ'
+global.owner_name = 'M O H A M E D    B E L L A'
 // Maximum upload file size limit (Default : 250 MB)
 global.max_upload = 250
 // Delay for spamming protection (Default : 3 seconds)
@@ -13,16 +13,16 @@ global.timer = 1800000
 // Symbols that are excluded when adding a prefix (Don't change it)
 global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
-global.blocks = ['91', '92', '62']
+global.blocks = ['91', '92']
 // Put target jid to forward friends story
-global.forwards = '212605784394@c.us'
+global.forwards = '212692516633@c.us'
 // Get neoxr apikey by registering at https://api.nxr.my.id
 global.Api = new (require('./neoxrApi'))('2lleJx')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
 global.chatai_bid = '164728'
 global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Footer text
-global.footer = 'ʙᴏʙɪᴢ ʙᴏᴛ ʙʏ ɴᴏᴜʀᴇᴅᴅɪɴᴇ ᴏᴜᴀꜰʏ'
+global.footer = 'B O T   BY  M O H A M E D   B E L L A'
 // Global status
 global.status = Object.freeze({
    wait: Func.texted('bold', 'المرجو الانتطار . . .'),
