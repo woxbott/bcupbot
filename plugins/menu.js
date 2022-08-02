e6e82b113e21fe3be4f97c496f7c7469877838a3
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-08-02 06:30:40 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,7 +9,7 @@ exports.run = {
          rowId: `${isPrefix}menutype 1`,
          description: ``
       }]
-      await client.sendList(m.chat, '', 'H E L L O', '© wox-bote v1 (Public Bot)', 'Tap!', [{
+      await client.sendList(m.chat, '', 'Check My Stories To Get daily Tricks 😏 \n\n Instagram.com/wox_bella', '© wox-bot v1 (Public Bot)', 'Tap!', [{
          rows
       }], m)
    },
