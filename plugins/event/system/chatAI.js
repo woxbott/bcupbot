2a943446fc83c1251695042de54ad5cc7b1e3846
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-08-02 02:11:24 +0100

    Update chatAI.js

--- plugins/event/system/chatAI.js
+++ plugins/event/system/chatAI.js
@@ -1,5 +1,5 @@
 exports.run = {
-    usage: ['hello', 'hello'],
+    usage: ['menu','ig','mediafire','apkgoogle','apk','play','yts','alloschool','pdfdrive','devbook','sticker','s'],
    async: async (m, {
       client,
       body,
@@ -9,7 +9,7 @@ exports.run = {
       prefixes
    }) => {
       try { 
-        if (command == 'hello')  {
+        if (command == 'menu' || command == 'ig' || command == 'mediafire' || command == 'apkgoogle' || command == 'apk' || command == 'play' || command == 'yts' || command == 'alloschool' || command == 'pdfdrive' || command == 'devbook'|| command == 'sticker'|| command == 's')  {
          client.reply(m.chat, `  غير مسموح لهذا الحساب باستخدام البوت ٠ تواصل مع صاحب البوت من اجل الانضمام للمجموعة wa.me/212690516633  `, m).then(async () => await client.updateBlockStatus(m.sender, 'block'))
         }
       } catch (e) {
